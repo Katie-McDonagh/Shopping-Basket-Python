20d1d1a2e13de98c7f153fb1d341459d69f53193
Katie McDonagh <mcdonagh.katie@outlook.com>
2020-11-05 17:36:19 +0000

    add basket class with pass

---
+++ test/shopping_basket_test.py
@@ -0,0 +1,12 @@
+import unittest
+
+class Basket(object):
+  pass
+
+class ShoppingBasketTest(unittest.TestCase):
+  def test_empty_basket_total(self):
+    basket = Basket([])
+
+
+if __name__ == '__main__'
+  pass
